a78703ddf17dc8ccd7668ff9b90d9413ef8d2998
Mai Duo Wu <wmaiduo@gmail.com>
2021-09-17 03:08:42 +0000

    completed addProperty

--- server/database.js
+++ server/database.js
@@ -207,9 +207,6 @@ const getAllProperties = function (options, limit = 10) {
    LIMIT $${queryParams.length};
    `;
 
-  // 5
-  console.log({ queryString, queryParams, options });
-
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 
@@ -232,9 +229,27 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  returning *;
+  `
+  const queryParams = [];
+  queryParams.push(property.owner_id);
+  queryParams.push(property.title);
+  queryParams.push(property.description);
+  queryParams.push(property.thumbnail_photo_url);
+  queryParams.push(property.cover_photo_url);
+  queryParams.push(property.cost_per_night);
+  queryParams.push(property.street);
+  queryParams.push(property.city);
+  queryParams.push(property.province);
+  queryParams.push(property.post_code);
+  queryParams.push(property.country);
+  queryParams.push(property.parking_spaces);
+  queryParams.push(property.number_of_bathrooms);
+  queryParams.push(property.number_of_bedrooms);
+
+  console.log({queryString, queryParams, property});
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.addProperty = addProperty;
